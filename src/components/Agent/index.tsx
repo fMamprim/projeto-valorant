30c1a63520df90ab2f2d17c8c7e3d0560df1756e
Felipe Mamprim <100654726+fMamprim@users.noreply.github.com>
2025-02-21 10:53:27 -0300

    Estrutura dos Cards

---
+++ src/components/Agent/index.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import styles from './style.module.scss'
+
+// criar uma interface pra colocar na const Agent e poder usar as propriedades de cada agent que está no json
+interface AgentProps {
+  agent: {
+    role: {
+      displayName: string
+    }
+    displayName: string
+    abilities: {
+      displayIcon: string
+      displayName: string
+    }[]
+    // esses 2 colchetes transformam isso em um array
+  }
+}
+
+export const Agent = ({ agent }: AgentProps) => (
+  <a href="#" className={styles.agent}>
+    <div>
+      <p>{agent.role.displayName}</p>
+      <strong>{agent.displayName}</strong>
+    </div>
+
+    <ul>
+      {agent.abilities.map(ability => (
+        // mapeia as habilidades, pois não é um array fixo, tem agente que tem 4 habilidades, tem uns que tem 3
+        // tem que colocar uma key única para renderizar cada elemento unicamente
+        <li key={ability.displayName}>
+          <Image
+            src={ability.displayIcon}
+            width={36}
+            height={36}
+            alt={'Ícone da habilidade ' + ability.displayName}
+          />
+        </li>
+      ))}
+    </ul>
+  </a>
+)
